a70822e0302df03f87de75d09212fb83f8e25738
sizzleistics <sizzlesdumbars@gmail.com>
2025-05-23 19:29:59 -0500

    Add files via upload

---
+++ output/buttons.cs
@@ -0,0 +1,24 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-05-24 00:27:04.557023300 UTC
+
+namespace CS2Dumper {
+    // Module: client.dll
+    public static class Buttons {
+        public const nint attack = 0x184B8F0;
+        public const nint attack2 = 0x184B980;
+        public const nint back = 0x184BBC0;
+        public const nint duck = 0x184BE90;
+        public const nint forward = 0x184BB30;
+        public const nint jump = 0x184BE00;
+        public const nint left = 0x184BC50;
+        public const nint lookatweapon = 0x1A71EE0;
+        public const nint reload = 0x184B860;
+        public const nint right = 0x184BCE0;
+        public const nint showscores = 0x1A71DC0;
+        public const nint sprint = 0x184B7D0;
+        public const nint turnleft = 0x184BA10;
+        public const nint turnright = 0x184BAA0;
+        public const nint use = 0x184BD70;
+        public const nint zoom = 0x1A71E50;
+    }
+}
---
+++ output/offsets.rs
@@ -0,0 +1,57 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-05-24 00:27:04.557023300 UTC
+
+#![allow(non_upper_case_globals, unused)]
+
+pub mod cs2_dumper {
+    pub mod offsets {
+        // Module: client.dll
+        pub mod client_dll {
+            pub const dwCSGOInput: usize = 0x1A71FC0;
+            pub const dwEntityList: usize = 0x19FEE38;
+            pub const dwGameEntitySystem: usize = 0x1B22848;
+            pub const dwGameEntitySystem_highestEntityIndex: usize = 0x20F0;
+            pub const dwGameRules: usize = 0x1A638A8;
+            pub const dwGlobalVars: usize = 0x1846EB0;
+            pub const dwGlowManager: usize = 0x1A62FE0;
+            pub const dwLocalPlayerController: usize = 0x1A4D840;
+            pub const dwLocalPlayerPawn: usize = 0x18530D0;
+            pub const dwPlantedC4: usize = 0x1A6D068;
+            pub const dwPrediction: usize = 0x1852F50;
+            pub const dwSensitivity: usize = 0x1A645C8;
+            pub const dwSensitivity_sensitivity: usize = 0x40;
+            pub const dwViewAngles: usize = 0x1A72390;
+            pub const dwViewMatrix: usize = 0x1A67FA0;
+            pub const dwViewRender: usize = 0x1A688F0;
+            pub const dwWeaponC4: usize = 0x1A01320;
+        }
+        // Module: engine2.dll
+        pub mod engine2_dll {
+            pub const dwBuildNumber: usize = 0x541BE4;
+            pub const dwNetworkGameClient: usize = 0x540CE0;
+            pub const dwNetworkGameClient_clientTickCount: usize = 0x368;
+            pub const dwNetworkGameClient_deltaTick: usize = 0x27C;
+            pub const dwNetworkGameClient_isBackgroundMap: usize = 0x281447;
+            pub const dwNetworkGameClient_localPlayer: usize = 0xF0;
+            pub const dwNetworkGameClient_maxClients: usize = 0x238;
+            pub const dwNetworkGameClient_serverTickCount: usize = 0x36C;
+            pub const dwNetworkGameClient_signOnState: usize = 0x228;
+            pub const dwWindowHeight: usize = 0x62457C;
+            pub const dwWindowWidth: usize = 0x624578;
+        }
+        // Module: inputsystem.dll
+        pub mod inputsystem_dll {
+            pub const dwInputSystem: usize = 0x387E0;
+        }
+        // Module: matchmaking.dll
+        pub mod matchmaking_dll {
+            pub const dwGameTypes: usize = 0x1A4190;
+            pub const dwGameTypes_mapName: usize = 0x120;
+        }
+        // Module: soundsystem.dll
+        pub mod soundsystem_dll {
+            pub const dwSoundSystem: usize = 0x3A15F0;
+            pub const dwSoundSystem_engineViewData: usize = 0x7C;
+        }
+    }
+}
