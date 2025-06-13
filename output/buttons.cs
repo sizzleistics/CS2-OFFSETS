81e1e9d1a0bc63ec50054bdb0e714cbd6642c997
sizzleistics <sizzlesdumbars@gmail.com>
2025-06-13 18:34:54 -0500

    Add files via upload

---
+++ output/buttons.cs
@@ -0,0 +1,24 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-06-13 23:31:41.043790800 UTC
+
+namespace CS2Dumper {
+    // Module: client.dll
+    public static class Buttons {
+        public const nint attack = 0x184E8F0;
+        public const nint attack2 = 0x184E980;
+        public const nint back = 0x184EBC0;
+        public const nint duck = 0x184EE90;
+        public const nint forward = 0x184EB30;
+        public const nint jump = 0x184EE00;
+        public const nint left = 0x184EC50;
+        public const nint lookatweapon = 0x1A75170;
+        public const nint reload = 0x184E860;
+        public const nint right = 0x184ECE0;
+        public const nint showscores = 0x1A75050;
+        public const nint sprint = 0x184E7D0;
+        public const nint turnleft = 0x184EA10;
+        public const nint turnright = 0x184EAA0;
+        public const nint use = 0x184ED70;
+        public const nint zoom = 0x1A750E0;
+    }
+}
