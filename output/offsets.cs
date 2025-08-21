1c3ea899c76f3521e0feb99f0ff9d12df4b73e04
sizzleistics <sizzlesdumbars@gmail.com>
2025-08-21 13:04:57 -0500

    Add files via upload

---
+++ output/offsets.cs
@@ -0,0 +1,39 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-08-21 18:04:00.250495100 UTC
+
+namespace CS2Dumper.Offsets {
+    // Module: client.dll
+    public static class ClientDll {
+        public const nint dwCSGOInput = 0x1E3CE10;
+        public const nint dwEntityList = 0x1D154C8;
+        public const nint dwGameRules = 0x1E31A68;
+        public const nint dwGlobalVars = 0x1BE6938;
+        public const nint dwGlowManager = 0x1E2E850;
+        public const nint dwPlantedC4 = 0x1E37B10;
+        public const nint dwViewMatrix = 0x1E32830;
+        public const nint dwViewRender = 0x1E33670;
+    }
+    // Module: engine2.dll
+    public static class Engine2Dll {
+        public const nint dwBuildNumber = 0x5E1144;
+        public const nint dwNetworkGameClient = 0x5E0230;
+        public const nint dwNetworkGameClient_clientTickCount = 0x358;
+        public const nint dwNetworkGameClient_deltaTick = 0x27C;
+        public const nint dwNetworkGameClient_maxClients = 0x230;
+        public const nint dwNetworkGameClient_serverTickCount = 0x23C;
+        public const nint dwNetworkGameClient_signOnState = 0x220;
+        public const nint dwWindowHeight = 0x8D93B4;
+        public const nint dwWindowWidth = 0x8D93B0;
+    }
+    // Module: inputsystem.dll
+    public static class InputsystemDll {
+    }
+    // Module: matchmaking.dll
+    public static class MatchmakingDll {
+        public const nint dwGameTypes_mapName = 0x120;
+    }
+    // Module: soundsystem.dll
+    public static class SoundsystemDll {
+        public const nint dwSoundSystem = 0x3DEB80;
+    }
+}
---
+++ output/offsets.hpp
@@ -0,0 +1,45 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-08-21 18:04:00.250495100 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace offsets {
+        // Module: client.dll
+        namespace client_dll {
+            constexpr std::ptrdiff_t dwCSGOInput = 0x1E3CE10;
+            constexpr std::ptrdiff_t dwEntityList = 0x1D154C8;
+            constexpr std::ptrdiff_t dwGameRules = 0x1E31A68;
+            constexpr std::ptrdiff_t dwGlobalVars = 0x1BE6938;
+            constexpr std::ptrdiff_t dwGlowManager = 0x1E2E850;
+            constexpr std::ptrdiff_t dwPlantedC4 = 0x1E37B10;
+            constexpr std::ptrdiff_t dwViewMatrix = 0x1E32830;
+            constexpr std::ptrdiff_t dwViewRender = 0x1E33670;
+        }
+        // Module: engine2.dll
+        namespace engine2_dll {
+            constexpr std::ptrdiff_t dwBuildNumber = 0x5E1144;
+            constexpr std::ptrdiff_t dwNetworkGameClient = 0x5E0230;
+            constexpr std::ptrdiff_t dwNetworkGameClient_clientTickCount = 0x358;
+            constexpr std::ptrdiff_t dwNetworkGameClient_deltaTick = 0x27C;
+            constexpr std::ptrdiff_t dwNetworkGameClient_maxClients = 0x230;
+            constexpr std::ptrdiff_t dwNetworkGameClient_serverTickCount = 0x23C;
+            constexpr std::ptrdiff_t dwNetworkGameClient_signOnState = 0x220;
+            constexpr std::ptrdiff_t dwWindowHeight = 0x8D93B4;
+            constexpr std::ptrdiff_t dwWindowWidth = 0x8D93B0;
+        }
+        // Module: inputsystem.dll
+        namespace inputsystem_dll {
+        }
+        // Module: matchmaking.dll
+        namespace matchmaking_dll {
+            constexpr std::ptrdiff_t dwGameTypes_mapName = 0x120;
+        }
+        // Module: soundsystem.dll
+        namespace soundsystem_dll {
+            constexpr std::ptrdiff_t dwSoundSystem = 0x3DEB80;
+        }
+    }
+}
