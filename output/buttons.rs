ea457c67ed6394f60a013df7deab8bcf2c471a63
sizzleistics <sizzlesdumbars@gmail.com>
2025-08-21 13:11:13 -0500

    Add files via upload

---
+++ output/buttons.rs
@@ -0,0 +1,26 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-08-21 18:09:40.489211400 UTC
+
+#![allow(non_upper_case_globals, unused)]
+
+pub mod cs2_dumper {
+    // Module: client.dll
+    pub mod buttons {
+        pub const attack: usize = 0x1BEAB30;
+        pub const attack2: usize = 0x1BEABC0;
+        pub const back: usize = 0x1BEAE00;
+        pub const duck: usize = 0x1BEB0D0;
+        pub const forward: usize = 0x1BEAD70;
+        pub const jump: usize = 0x1BEB040;
+        pub const left: usize = 0x1BEAE90;
+        pub const lookatweapon: usize = 0x1E3CD30;
+        pub const reload: usize = 0x1BEAAA0;
+        pub const right: usize = 0x1BEAF20;
+        pub const showscores: usize = 0x1E3CC10;
+        pub const sprint: usize = 0x1BEAA10;
+        pub const turnleft: usize = 0x1BEAC50;
+        pub const turnright: usize = 0x1BEACE0;
+        pub const r#use: usize = 0x1BEAFB0;
+        pub const zoom: usize = 0x1E3CCA0;
+    }
+}
