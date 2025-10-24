a1dc46e110a3c2793ec058af96da7dd8d9a2bd93
sizzleistics <sizzlesdumbars@gmail.com>
2025-10-24 14:02:38 -0500

    Add files via upload

---
+++ output/panorama_dll.cs
@@ -0,0 +1,14 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-10-24 19:00:33.897479700 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: panorama.dll
+    // Class count: 1
+    // Enum count: 0
+    public static class PanoramaDll {
+        // Parent: None
+        // Field count: 0
+        public static class ELayoutNodeType {
+        }
+    }
+}
---
+++ output/panorama_dll.hpp
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-10-24 19:00:33.897479700 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: panorama.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace panorama_dll {
+            // Parent: None
+            // Field count: 0
+            namespace ELayoutNodeType {
+            }
+        }
+    }
+}
